8f3426ee6696802e1670d86c9d0537e40c74e1a7
Benjamin Prieur <benjamin@prieur.org>
2020-08-18 16:49:06 +0200

    Mutualize http request

--- go.mod
+++ go.mod
@@ -4,11 +4,8 @@ go 1.14
 
 require (
 	github.com/aws/aws-sdk-go v1.32.9
-	github.com/bugsnag/bugsnag-go v1.5.3
-	github.com/bugsnag/panicwrap v1.2.0 // indirect
 	github.com/gin-gonic/gin v1.6.2
-	github.com/gofrs/uuid v3.3.0+incompatible // indirect
-	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/newrelic/go-agent/v3 v3.8.1
 	github.com/sirupsen/logrus v1.5.0
-	go.mongodb.org/mongo-driver v1.3.2
+	go.mongodb.org/mongo-driver v1.3.5
 )
---
+++ helpers/http.request.go
@@ -0,0 +1,138 @@
+package helpers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	newrelic "github.com/newrelic/go-agent/v3/newrelic"
+)
+
+var client *http.Client
+
+func init() {
+	client = &http.Client{}
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+	http.DefaultTransport.(*http.Transport).MaxIdleConns = 300
+}
+
+func GetJson(c context.Context, url string, expectedCodeHTTPResponseCode int, target interface{}) error {
+	req, err := http.NewRequest("GET", url, nil)
+	txn := newrelic.FromContext(c)
+	txn.AddAttribute("url_full", url)
+	s := newrelic.StartExternalSegment(txn, req)
+	resp, err := client.Do(req)
+	s.Response = resp
+	s.End()
+	if err != nil {
+		return NewHTTPError(err, http.StatusInternalServerError, "Unexpected error occurred")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != expectedCodeHTTPResponseCode {
+		var responseError map[string]interface{}
+		if err = json.NewDecoder(resp.Body).Decode(&responseError); err != nil {
+			txn.AddAttribute("err", err.Error())
+			return NewHTTPError(err, resp.StatusCode, "Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode))
+		}
+		err = fmt.Errorf("Unexpected response from back API - %v: %v", resp.StatusCode, responseError)
+		txn.AddAttribute("err", err.Error())
+		return NewHTTPError(
+			err,
+			resp.StatusCode,
+			"Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode),
+		)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(target)
+	if err != nil {
+		return NewHTTPError(err, http.StatusBadRequest, "Failed during parsing back api response")
+	}
+
+	return nil
+}
+
+func PostJson(c context.Context, url string, expectedCodeHTTPResponseCode int, data, target interface{}) error {
+	bodyBytes, _ := json.Marshal(data)
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(bodyBytes))
+	req.Header.Set("Content-Type", "application/json")
+	txn := newrelic.FromContext(c)
+	txn.AddAttribute("body", string(bodyBytes))
+	s := newrelic.StartExternalSegment(txn, req)
+	resp, err := client.Do(req)
+	s.Response = resp
+	s.End()
+	if err != nil {
+		return NewHTTPError(err, http.StatusInternalServerError, "Unexpected error occurred")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != expectedCodeHTTPResponseCode {
+		var responseError map[string]interface{}
+		if err = json.NewDecoder(resp.Body).Decode(&responseError); err != nil {
+			txn.AddAttribute("err", err.Error())
+			return NewHTTPError(err, resp.StatusCode, "Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode))
+		}
+		err = fmt.Errorf("Unexpected response from back API - %v: %v", resp.StatusCode, responseError)
+		txn.AddAttribute("err", err.Error())
+		return NewHTTPError(
+			err,
+			resp.StatusCode,
+			"Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode),
+		)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(target)
+	if err != nil {
+		return NewHTTPError(err, http.StatusBadRequest, "Failed during parsing back api response")
+	}
+
+	return nil
+}
+
+func DeleteJson(c context.Context, url string, expectedCodeHTTPResponseCode int, data, target interface{}) error {
+	bodyBytes, _ := json.Marshal(data)
+
+	// Create request
+	req, err := http.NewRequest("DELETE", url, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return NewHTTPError(err, http.StatusInternalServerError, "Unexpected error occurred")
+	}
+
+	txn := newrelic.FromContext(c)
+	txn.AddAttribute("body", string(bodyBytes))
+	s := newrelic.StartExternalSegment(txn, req)
+	req.Header.Set("Content-Type", "application/json")
+
+	// Fetch Request
+	resp, err := client.Do(req)
+	s.Response = resp
+	s.End()
+	if err != nil {
+		return NewHTTPError(err, http.StatusInternalServerError, "Unexpected error occurred")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != expectedCodeHTTPResponseCode {
+		var responseError map[string]interface{}
+		if err = json.NewDecoder(resp.Body).Decode(&responseError); err != nil {
+			txn.AddAttribute("err", err.Error())
+			return NewHTTPError(err, resp.StatusCode, "Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode))
+		}
+		err = fmt.Errorf("Unexpected response from back API - %v: %v", resp.StatusCode, responseError)
+		txn.AddAttribute("err", err.Error())
+		return NewHTTPError(
+			err,
+			resp.StatusCode,
+			"Unexpected response from back API - StatusCode: "+strconv.Itoa(resp.StatusCode),
+		)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(target)
+	if err != nil {
+		return NewHTTPError(err, http.StatusBadRequest, "Failed during parsing back api response")
+	}
+
+	return nil
+}
